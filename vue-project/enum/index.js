832e2c0c83d629d58cc73001a5a280bd3777d1b4
wuricong <15070301734@163.com>
2025-10-13 09:23:42 +0800

    feat:书签优化

--- vue-project/enum/index.js
+++ vue-project/enum/index.js
@@ -1,16 +1,38 @@
 export const primaryUrlEnum = [
-  {name:'MDN',url:'https://developer.mozilla.org/zh-CN/',id:1,svg:'mdn'},
-  {name:'vite',url:'https://cn.vite.dev/guide/',id:2,svg:'vite'},
-  {name:'element-ui',url:'https://element.eleme.cn/#/zh-CN',id:3,svg:'element'},
-  {name:'ChatGPT',url:'https://chatgpt.com/?model=auto',id:4,svg:'ChatGPT'},
-  {name:'elementPlus',url:'https://element-plus.org/zh-CN/',id:5,svg:'element-plus'}
-]
+  {
+    name: "MDN",
+    url: "https://developer.mozilla.org/zh-CN/",
+    id: 1,
+    svg: "mdn",
+  },
+  { name: "vite", url: "https://cn.vite.dev/guide/", id: 2, svg: "vite" },
+  {
+    name: "element-ui",
+    url: "https://element.eleme.cn/#/zh-CN",
+    id: 3,
+    svg: "element",
+  },
+  {
+    name: "ChatGPT",
+    url: "https://chatgpt.com/?model=auto",
+    id: 4,
+    svg: "ChatGPT",
+  },
+  {
+    name: "elementPlus",
+    url: "https://element-plus.org/zh-CN/",
+    id: 5,
+    svg: "element-plus",
+  },
+  { name: "google", url: "https://www.google.com/", id: 6, svg: "google" },
+  { name: "juejin", url: "https://juejin.cn/", id: 7, svg: "juejin" },
+  { name: "github", url: "https://github.com/wuricong", id: 7, svg: "github" },
+];
 
-export const workUrlEnum = [
-
-]
-
+export const workUrlEnum = [];
 
 export const engineSearchEnum = [
-  {name:'google',url:'https://www.google.com/search?q=',id:1}
-]
+  { name: "google", url: "https://www.google.com/search?q=", id: 1 },
+];
+
+const searchTip = "https://www.google.com/complete/search?q=12";
--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,44 +1,99 @@
 <script setup>
-import {onMounted, ref} from "vue";
-import {primaryUrlEnum} from "../enum";
+import { onMounted, ref } from "vue"
+import { primaryUrlEnum } from "../enum"
+import { DeleteOutlined } from "@ant-design/icons-vue"
+import _ from "lodash"
 
 const bookmarks = ref([])
 const searchValue = ref()
+const devItems = ref([
+  {
+    title: "大兴后台",
+    id: 1,
+    url: "http://192.168.0.222:8080/view/daxing_dev/job/dev_daxin_console_web/",
+  },
+  {
+    title: "大兴商城端",
+    id: 2,
+    url: "http://192.168.0.222:8080/view/daxing_dev/job/dev_daxin_mall_web/",
+  },
+  {
+    title: "公共版后台",
+    id: 3,
+    url: "http://192.168.0.222:8080/view/prod/job/prod_zpc_mall_web_console/build?delay=0sec",
+  },
+  {
+    title: "公共版商城端",
+    id: 4,
+    url: "http://192.168.0.222:8080/view/prod/job/prod_zpc_mall_web/build?delay=0sec",
+  },
+])
 
 const doOpenTab = (item) => {
-  window.open(item.url,'_blank')
+  window.open(item.url, "_blank")
 }
 
-onMounted(()=>{
+const handleSearchChange = _.debounce(() => {
+  fetch("http://8.141.112.69:3000/hello").then((res) => {
+    console.log("res", res)
+  })
+}, 300)
+
+function getBooks(bookList, books) {
+  for (const book of bookList) {
+    if (book.children) {
+      getBooks(book.children, books)
+    } else {
+      books.push(book)
+    }
+  }
+}
+
+const delBook = (row) => {
+  chrome.bookmarks.remove(row.id)
+  refreshBookMarks()
+}
+
+const refreshBookMarks = () => {
   chrome.bookmarks.getTree((bookmarkTreeNodes) => {
-    bookmarks.value = bookmarkTreeNodes[0].children.reduce((cur,prev)=>{
-      return [...cur,...prev.children]
-    },[])
+    let books = []
+    getBooks(bookmarkTreeNodes, books)
+    bookmarks.value = books
+  })
+}
 
-    console.log('bookmarks.value',bookmarks.value)
-  });
+const filterBookMark = () => {}
+
+onMounted(() => {
+  refreshBookMarks()
 })
-
 </script>
 
 <template>
   <div class="mask"></div>
-  <a-input style="width: 300px" placeholder="请输入要搜索的内容" v-model="searchValue"/>
+  <a-input style="width: 300px" @input="handleSearchChange" placeholder="请输入要搜索的内容" v-model="searchValue" />
   <div class="py-2 px-2">
-    <div class="mb-2" style="cursor: pointer">主要网站</div>
     <div class="flex gap-2 mb-2">
-      <div  v-for="item in primaryUrlEnum" :key="item.id" @click="doOpenTab(item)" class="cursor-pointer">
-        <svg-icon :name="item.svg"/>
+      <div v-for="item in primaryUrlEnum" :key="item.id" @click="doOpenTab(item)" class="cursor-pointer">
+        <svg-icon :name="item.svg" />
       </div>
     </div>
-    <div class="py-2">
+    <div class="py-2 flex justify-between">
       <div class="book-container w-1/3">
         <div class="book-inner">
-          <div class="book" v-for="item in bookmarks" :key="item.id" @click="doOpenTab(item)">
-            {{item.title}}
+          <div class="book flex justify-between items-center" v-for="item in bookmarks" :key="item.id" @click="doOpenTab(item)">
+            <div class="flex-grow">{{ item.title }}</div>
+            <DeleteOutlined class="ml-2" @click.stop="delBook(item)" />
+          </div>
+          <i></i>
+        </div>
+      </div>
+      <div class="book-container w-1/5">
+        <div class="book-inner">
+          <div class="book" v-for="item in devItems" :key="item.id" @click="doOpenTab(item)">
+            {{ item.title }}
           </div>
         </div>
-
       </div>
     </div>
   </div>
@@ -46,11 +101,12 @@ onMounted(()=>{
 
 <style scoped lang="scss">
 .web-tag:hover {
-  background-color:#293e6b !important;
+  background-color: #293e6b !important;
 }
 
-.book-container{
-  color: #CAD2DA;
+.book-container {
+  flex-shrink: 0;
+  color: #cad2da;
   background: rgba(0, 0, 0, 0.4); /* 半透明白色背景 */
   backdrop-filter: blur(10px); /* 设置背景模糊效果 */
   -webkit-backdrop-filter: blur(10px); /* Safari 支持 */
@@ -63,7 +119,8 @@ onMounted(()=>{
   min-height: 300px;
   max-height: 600px;
   overflow: auto;
-  .book{
+
+  .book {
     margin: 8px 0;
     font-size: 14px;
     font-weight: 500;
@@ -72,8 +129,8 @@ onMounted(()=>{
     padding: 4px 8px;
   }
 
-  .book:hover{
-    background: rgba(0, 0, 0, 0.5)
+  .book:hover {
+    background: rgba(0, 0, 0, 0.5);
   }
 }
 </style>
