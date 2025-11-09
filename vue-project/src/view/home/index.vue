3530a3d38738b960b32b9ccd36a61b13041defab
wuricong <15070301734@163.com>
2025-11-09 21:31:04 +0800

    feat:搜索引擎自定义

--- vue-project/src/view/home/index.vue
+++ vue-project/src/view/home/index.vue
@@ -5,11 +5,20 @@ import { EllipsisOutlined } from "@ant-design/icons-vue"
 import Control from "@/view/control/index.vue"
 import _ from "lodash"
 import BookModal from "@/view/home/modal/book-modal.vue"
+import SvgIcon from "@/svg/svg-icon.vue"
 
 const searchValue = ref()
 const devItems = ref(devItemsEnum)
 const controlRef = ref()
 const showBookModal = ref(false)
+const searchEngineEnum = ref([
+  { label: "谷歌", value: "https://www.google.com/search?q", svg: "google", id: 1 },
+  { label: "github", value: " https://github.com/search?q", svg: "github", id: 2 },
+  { label: "必应", value: "https://cn.bing.com/search?q", svg: "bing", id: 3 },
+  { label: "掘金", value: " https://juejin.cn/search?query", svg: "juejin", id: 4 },
+  { label: "百度", value: "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd", svg: "baidu", id: 5 },
+])
+const curEngine = ref(1)
 
 const doOpenTab = (item) => {
   window.open(item.url, "_blank")
@@ -33,33 +42,39 @@ const handleProdUrl = (item) => {
 }
 
 const handleSearch = () => {
-  window.open(`https://www.google.com/search?q=${searchValue.value}`)
+  const index = searchEngineEnum.value.findIndex((item) => item.id === curEngine.value)
+  const url = searchEngineEnum.value[index]
+  window.open(`${url.value}=${searchValue.value}`)
 }
 </script>
 
 <template>
-  <div class="pt-2">
-    <a-input-search
-      size="middle"
-      style="width: 260px"
-      @input="handleSearchChange"
-      @keydown.enter="handleSearch"
-      placeholder="请输入要搜索的内容"
-      v-model:value="searchValue"
-    >
-      <template #enterButton>
-        <a-button @click="handleSearch" type="primary" size="middle">搜索</a-button>
-      </template>
-    </a-input-search>
+  <div class="pt-2 px-2">
+    <a-input-group compact style="width: 620px">
+      <a-select style="width: 60px" v-model:value="curEngine">
+        <a-select-option v-for="item in searchEngineEnum" :value="item.id" :key="item.id">
+          <svg-icon style="height: 16px" :name="item.svg" />
+        </a-select-option>
+      </a-select>
+      <a-input
+        @input="handleSearchChange"
+        @keydown.enter="handleSearch"
+        placeholder="请输入要搜索的内容"
+        v-model:value="searchValue"
+        style="width: calc(100% - 200px); border-left: none"
+      />
+      <a-button @click="handleSearch" type="primary" size="middle">搜索</a-button>
+    </a-input-group>
 
-    <div class="py-2 px-2">
+    <div class="py-2">
       <div class="flex gap-2 mb-2 primary-enum-main">
         <div v-for="item in primaryUrlEnum" :key="item.id" @click="doOpenTab(item)" class="cursor-pointer" :style="item.style || ''">
           <svg-icon :style="item.svgStyle" :name="item.svg" />
         </div>
+        <div class="book-svg">
+          <svg-icon :name="'book'" @click="handleBookOpen" class="cursor-pointer" />
+        </div>
       </div>
-
-      <svg-icon :name="'book'" @click="handleBookOpen" class="cursor-pointer" />
 
       <div class="py-2 flex justify-between">
         <div class="devurl-container">
@@ -159,5 +174,16 @@ const handleSearch = () => {
   background: rgba(0, 0, 0, 0.1); /* 半透明背景（浅色系） */
   -webkit-backdrop-filter: blur(10px) saturate(120%);
   backdrop-filter: blur(10px) saturate(120%);
+}
+
+:deep(.ant-select-selection-item) {
+  display: flex;
+  align-items: center;
+}
+
+.book-svg {
+  margin-left: 8px;
+  padding-left: 12px;
+  border-left: 1px solid #ffffff;
 }
 </style>
