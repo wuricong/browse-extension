2d232020ccdfe6a43cc89df13aebe2d79c16c7fa
wuricong <15070301734@163.com>
2026-03-04 11:35:08 +0800

    feat:优化
    Signed-off-by: wuricong <15070301734@163.com>

--- vue-project/src/components/button-group/index.vue
+++ vue-project/src/components/button-group/index.vue
@@ -8,11 +8,11 @@
       <div>hover</div>
     </div>
     <div>
-      <button>按钮</button>
-      <div class="button-active">active</div>
+      <button class="button-active">按钮</button>
+      <div>active</div>
     </div>
     <div>
-      <button class="button-disable">按钮</button>
+      <button class="button-disable" :disabled="true">按钮</button>
       <div>disable</div>
     </div>
   </div>
@@ -24,14 +24,19 @@ button {
   width: 36px;
   border-radius: 4px;
   color: #fff;
+  transition: all 0.5s;
 }
 
-.button-hover {
+.button-hover:hover {
+  filter: brightness(1.2);
 }
 
-.button-active {
+.button-active:active {
+  filter: brightness(0.8);
 }
 
 .button-disable {
+  opacity: 0.7;
+  cursor: not-allowed;
 }
 </style>
